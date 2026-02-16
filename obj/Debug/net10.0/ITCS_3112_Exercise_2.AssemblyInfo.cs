2e8c677011a19c2a6a4b566ad62792636fb52df4
Keilee Wright <kwrigh95@charlotte.edu>
2026-02-16 16:17:16 -0500

    Adding the general folders.

--- obj/Debug/net10.0/ITCS_3112_Exercise_2.AssemblyInfo.cs
+++ obj/Debug/net10.0/ITCS_3112_Exercise_2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITCS_3112_Exercise_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f597fd48e10847a22e58a819011945107b559a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITCS_3112_Exercise_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITCS_3112_Exercise_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

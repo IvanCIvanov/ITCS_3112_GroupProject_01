e6f48241f7b1218748b1d1569dacf07db36f1abd
Keilee Wright <kwrigh95@charlotte.edu>
2026-02-22 23:42:42 -0500

    Added xml doc for Policy.cs

--- Domain/Policy.cs
+++ Domain/Policy.cs
@@ -1,7 +1,21 @@
 ﻿namespace ITCS_3112_Exercise_2.Domain;
 
+/// <summary>
+/// Creates an instance class for Policy, which will dictate the business rules
+/// of the classes.
+/// </summary>
 public class Policy : IPolicy
 {
+    /// <summary>
+    /// Dictates whether an item can or cannot be checked out depending on
+    /// their status.
+    /// </summary>
+    /// <param name="item">
+    /// Item being checked out.
+    /// </param>
+    /// <returns>
+    /// Returns True if the item can be checked out and False if not.
+    /// </returns>
     public bool CanCheckout(Item item)
     {
         // Business Rule: Only available items with known conditions can be checked out.

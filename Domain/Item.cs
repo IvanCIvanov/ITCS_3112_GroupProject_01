7a8f9ab6c1635a38ac90f29a1c695e8fed6c713a
Keilee Wright <kwrigh95@charlotte.edu>
2026-02-22 23:46:48 -0500

    Added xml doc for Item.cs

--- Domain/Item.cs
+++ Domain/Item.cs
@@ -14,6 +14,24 @@ namespace ITCS_3112_Exercise_2.Domain;
         public string Damages { get; set; }
         
         
+        /// <summary>
+        /// Creates an item that can be used in the repository.
+        /// </summary>
+        /// <param name="id">
+        /// Id of the item.
+        /// </param>
+        /// <param name="name">
+        /// Name of the item.
+        /// </param>
+        /// <param name="type">
+        /// Category of the item.
+        /// </param>
+        /// <param name="status">
+        /// Availability of the item.
+        /// </param>
+        /// <param name="condition">
+        /// Condition of the item.
+        /// </param>
         public Item(long id, string name, TypeEnum type, StatusEnum status, ConditionEnum condition){
             Id = id;
             Name = name;
@@ -22,6 +40,13 @@ namespace ITCS_3112_Exercise_2.Domain;
             Condition = condition;
         }
 
+        /// <summary>
+        /// Acts to override and return a string version regardless of original
+        /// type.
+        /// </summary>
+        /// <returns>
+        /// String receipt dictated by its insides.
+        /// </returns>
         public override string ToString()
         {
             return $"{Id} | {Name} | {Type} | {Status} | {Condition} ";

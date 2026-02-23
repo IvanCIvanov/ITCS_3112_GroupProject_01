e19ab92a0ad618adc9f86976f14b397218325501
Keilee Wright <kwrigh95@charlotte.edu>
2026-02-22 23:44:19 -0500

    Added xml doc for the Person.cs

--- Domain/People.cs
+++ Domain/People.cs
@@ -14,6 +14,19 @@
         public string Email { get; set; }
 
 
+        /// <summary>
+        /// Creates a Person based on the details given by the user.
+        /// </summary>
+        /// <param name="id">
+        /// The id of the user.
+        /// </param>
+        /// <param name="name">
+        /// The name of the user.
+        /// </param>
+        /// <param name="email">
+        ///
+        /// The email of the user.
+        /// </param>
         protected Person(long id, string name, string email)
         {
             Id = id;

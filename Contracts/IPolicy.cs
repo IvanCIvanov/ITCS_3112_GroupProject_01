6be5fae1a8edbbd0cf65c17f854339b99b9a49a2
Keilee Wright <kwrigh95@charlotte.edu>
2026-02-22 23:54:47 -0500

    Added xml doc for IPolicy.

--- Contracts/IPolicy.cs
+++ Contracts/IPolicy.cs
@@ -1,7 +1,19 @@
 ﻿using ITCS_3112_Exercise_2.Domain;
 namespace ITCS_3112_Exercise_2;
 
+/// <summary>
+/// Dictates how the item will be treated according to its status.
+/// </summary>
 public interface IPolicy
 {
+    /// <summary>
+    /// Shows whether an item can or cannot be checkedout.
+    /// </summary>
+    /// <param name="item">
+    /// Item that is being checked out.
+    /// </param>
+    /// <returns>
+    /// Returns True if can be checked out and False otherwise.
+    /// </returns>
     bool CanCheckout(Item item);
 }

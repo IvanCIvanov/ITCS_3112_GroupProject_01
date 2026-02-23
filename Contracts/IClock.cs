86cf97321fd6cd8fa7263572871c32970c6611fe
Keilee Wright <kwrigh95@charlotte.edu>
2026-02-22 23:52:58 -0500

    Updated the xml doc for IClock.

--- Contracts/IClock.cs
+++ Contracts/IClock.cs
@@ -1,6 +1,15 @@
 ﻿namespace ITCS_3112_Exercise_2.Contracts;
 
+/// <summary>
+/// Used as a reference point in the CheckoutService.
+/// </summary>
 public interface IClock
 {
+    /// <summary>
+    /// Gives the current time that is used as a refernce in CheckoutService.
+    /// </summary>
+    /// <returns>
+    /// Returns the current time.
+    /// </returns>
     public DateTime GetTime();
 }

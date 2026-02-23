c46a87a014ece5129a197cfb4f5ee7d185e0ce98
Keilee Wright <kwrigh95@charlotte.edu>
2026-02-22 23:50:55 -0500

    Added xml doc for the ICatalog.

--- Contracts/ICatalog.cs
+++ Contracts/ICatalog.cs
@@ -1,10 +1,36 @@
 ﻿using ITCS_3112_Exercise_2.Domain;
 namespace ITCS_3112_Exercise_2.Contracts;
 
+/// <summary>
+/// Showcases different ways the Catalog is altered throughout the process.
+/// </summary>
 public interface ICatalog
 {
+    /// <summary>
+    /// Shows all available items within the repository.
+    /// </summary>
+    /// <returns>
+    /// List of available items.
+    /// </returns>
     List<Item> GetAvailableItems();
+    
+    /// <summary>
+    /// Shows all unavailable items in the repository.
+    /// </summary>
+    /// <returns>
+    /// List of unavailable items.
+    /// </returns>
     List<Item> GetUnavailableItems();
+    
+    /// <summary>
+    /// Returns the query of the item being searched.
+    /// </summary>
+    /// <param name="query">
+    /// User input that is checked against the list.
+    /// </param>
+    /// <returns>
+    /// Information regarding the item being searched.
+    /// </returns>
     List<Item> Search(string query);
     
     

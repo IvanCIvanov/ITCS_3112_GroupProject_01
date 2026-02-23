fbc878336f7adff5acdbf547a761d5bebe9df34d
Keilee Wright <kwrigh95@charlotte.edu>
2026-02-22 23:31:46 -0500

    Updated xml doc for SystemClock.cs

--- Services/SystemClock.cs
+++ Services/SystemClock.cs
@@ -1,8 +1,17 @@
 ﻿using ITCS_3112_Exercise_2.Contracts;
 namespace ITCS_3112_Exercise_2.Services;
 
+/// <summary>
+/// Used to create a current time reference for checkouts.
+/// </summary>
 public class SystemClock : IClock
 {
+    /// <summary>
+    /// Returns the current time.
+    /// </summary>
+    /// <returns>
+    /// Returns the current time.
+    /// </returns>
     public DateTime GetTime()
     {
         return new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);

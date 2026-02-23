d40d2e813b3da718b1b71ba5ec702cf5c769d57b
Keilee Wright <kwrigh95@charlotte.edu>
2026-02-22 23:28:46 -0500

    Added xml documentation for iNotifier.cs

--- Contracts/INotifier.cs
+++ Contracts/INotifier.cs
@@ -1,8 +1,27 @@
 ﻿using ITCS_3112_Exercise_2.Domain;
 namespace ITCS_3112_Exercise_2;
 
+
+/// <summary>
+/// Defines the notification behavior for checkouts.
+/// These are how users are alerted when their due date is coming soon
+/// or if they have passed the threshold.
+/// </summary>
 public interface INotifier
 {
+    /// <summary>
+    /// Sends an alert when the due date is within 24 hours.
+    /// </summary>
+    /// <param name="record">
+    /// Checkout record containing all details about item.
+    /// </param>
     void AlertDueSoon(CheckoutRecord record);
+    
+    /// <summary>
+    /// Sends an alert for items that are overdue.
+    /// </summary>
+    /// <param name="record">
+    /// Checkout record representing the overdue item.
+    /// </param>
     void AlertOverdue(CheckoutRecord record);
 }

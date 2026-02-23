8b6095e84faf219ec4fbd57c97bb1fdc13efc0f6
Keilee Wright <kwrigh95@charlotte.edu>
2026-02-22 23:07:59 -0500

    Added xmk documentation for the CheckoutRecord.cs

--- Domain/CheckoutRecord.cs
+++ Domain/CheckoutRecord.cs
@@ -1,5 +1,11 @@
 ﻿namespace ITCS_3112_Exercise_2.Domain;
 
+/// <summary>
+/// CheckoutRecord exists to keep track of user requests.
+/// It utilizes CheckoutDate, DueDate, Borrower, Item, and Receipt
+/// to give the user an accurate representation of the item being checked
+/// out.
+/// </summary>
 public class CheckoutRecord
 {
     public DateTime CheckoutDate { get; set; }
@@ -8,6 +14,24 @@ public class CheckoutRecord
     public Item Item { get; set; }
     public Receipt Receipt { get; private set; }
     
+    
+    /// <summary>
+    /// Constructor for the CheckoutRecord class.
+    /// Represents a completed checkout transaction that ties together an item,
+    ///  borrower, dueDate, and summary.
+    /// </summary>
+    /// <param name="item">
+    /// Item being checked out
+    /// </param>
+    /// <param name="borrower">
+    /// Person borrowing the item
+    /// </param>
+    /// <param name="dueDate">
+    /// Associated due date the item must be returned.
+    /// </param>
+    /// <param name="summary">
+    /// Complete summary that ties everything together.
+    /// </param>
     public CheckoutRecord(Item item, Person borrower, DateTime dueDate, string summary)
     {
         Item = item;
